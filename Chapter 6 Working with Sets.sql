dda41ac42317e37ec67994440398e40393f6b36b
Christina Yarochkina <christina.yarochkina@gmail.com>
2020-05-09 18:27:26 +0300

    New changes

--- Chapter 6 Working with Sets.sql
+++ Chapter 6 Working with Sets.sql
@@ -1,9 +1,9 @@
-/* 6.1  Имеются множество A = {L M N O P} и множество B = {P Q R S T}. Какие мно􏰀жества будут получены в результате следующих операций:
-• A union B {L M N O P  Q R S T}
-• A union all B {L M N O P P Q R S T}
-• A intersect B {P}
-• A except B {L M N O}
-*/
+-- 6.1  Имеются множество A = {L M N O P} и множество B = {P Q R S T}. Какие мно􏰀жества будут получены в результате следующих операций:
+A union B {L M N O P  Q R S T}
+A union all B {L M N O P P Q R S T}
+A intersect B {P}
+A except B {L M N O}
+
 
 -- 6.2 Напишите составной запрос для выбора имен и фамилий всех клиен􏰀тов 􏰀физических лиц, а также имен и фамилий всех сотрудников.
 SELECT fname, lname
